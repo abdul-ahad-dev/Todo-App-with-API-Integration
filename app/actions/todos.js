c338f944fa9eb983cb52348c55604415664b4a82
abdulahad0110 <abdulahad200325@gmail.com>
2024-11-15 17:48:03 +0500

    first commit

---
+++ app/actions/todos.js
@@ -0,0 +1,5 @@
+export default function addTodo(formData) {
+    const todo = formData.get("todo");
+    console.log("form data==>", todo)
+    
+}
--- app/todo/page.js
+++ app/todo/page.js
@@ -1,3 +1,4 @@
+import TodoForm from "@/components/TodoForm";
 import { Button } from "@/components/ui/button";
 import {
   Table,
@@ -8,7 +9,7 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table"
-import { FilePenLine, Indent, Trash2 } from "lucide-react";
+import { FilePenLine, Trash2 } from "lucide-react";
 
 async function Todo() {
   let res = await fetch("http://localhost:3000/api/todos");
@@ -18,37 +19,40 @@ async function Todo() {
     <>
       <div className="text-3xl p-3 text-center font-semibold font-mono">Todo</div>
 
+      <TodoForm />
+
       <Table className="md:w-11/12 mx-auto border rounded-xl overflow-hidden shadow-lg">
-        <TableCaption>A list of your recent invoices.</TableCaption>
+        <TableCaption>A list of your recent todos.</TableCaption>
         <TableHeader>
           <TableRow>
-            <TableHead className="flex-none w-[100px]">S No.</TableHead>
+            <TableHead className="text-center w-[100px]">S No.</TableHead>
             <TableHead className="shrink text-center w-[60%]">Todos</TableHead>
-            <TableHead className="w-[180px] bg-slate-100 text-center">Status</TableHead>
+            <TableHead className="w-[180px] text-center">Status</TableHead>
             <TableHead className="text-center">Actions</TableHead>
           </TableRow>
         </TableHeader>
         <TableBody>
           {res?.todos?.map((item, index) => (
             <TableRow key={index}>
-              <TableCell className="font-medium">{index}</TableCell>
+              <TableCell className="text-center">{index}</TableCell>
               <TableCell className="text-center ">{item.todo}</TableCell>
               <TableCell className="text-center capitalize">
-                {item.status === "active" ? (
-                  <div className="bg-green-500 text-white rounded-full px-2 py-1 text-xs">
+                {item.isComplete ? (
+                  <div className="bg-green-500 text-white rounded-full px-2 py-1 text-xs cursor-pointer active:scale-95 select-none">
                     Active
                   </div>
                 ) : (
-                  <div className="bg-yellow-500 text-white rounded-full px-2 py-1 text-xs">
+                  <div className="bg-yellow-500 text-white rounded-full px-2 py-1 text-xs cursor-pointer active:scale-95 select-none">
                     Pending
                   </div>
                 )}
               </TableCell>
               <TableCell className="flex justify-center gap-2">
-                <Button variant="outline" size="icon">
+                <Button variant="outline" size="icon" className="active:scale-95">
                   <FilePenLine className="h-4 w-4" />
                 </Button>
-                <Button variant="outline" size="icon">
+
+                <Button variant="destructive" size="icon" className="active:scale-95">
                   <Trash2 className="h-4 w-4" />
                 </Button>
               </TableCell>
@@ -56,7 +60,6 @@ async function Todo() {
           ))}
         </TableBody>
       </Table>
-
     </>
   )
 }
